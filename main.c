e43f13cd43c9d6a3e56a1c00eed2bd0f4c6d232c
GGLVXD <gglvmax@gmail.com>
2024-05-11 00:22:31 +0300

    Added loading new game data

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include "sys/gameloadnew.h"
 
 int main(){
+    int gameoption;
     printf("Welcome to mandatory!");
+    printf("1. Start a new game (will overwrite current save)");
+    printf("2.load exisiting game");
+    printf("choose number from 1 to 2: ");
+    scanf("%d", &gameoption);
+    if(gameoption == 1) {
+    printf("Starting a new game...");
+    LoadNewGame();
+    }
 }
---
+++ sys/gameloadnew.h
@@ -0,0 +1,12 @@
+#ifndef gameloadnew_h
+#define gameloadnew_h
+#include <stdio.h>
+
+int LoadNewGame() {
+FILE *file_pointer;
+    file_pointer = fopen("gamedata.txt", "w");
+    FILE *file = fopen("gamedata.txt", "w");
+
+}
+
+#endif
